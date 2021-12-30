b60e4c79ded9eb08734d8b3831727f93ebcf6e60
wizmain <wizmain@gmail.com>
2021-12-30 13:40:12 +0900

    init

---
+++ gradient_simplenet.py
@@ -0,0 +1,22 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3) #정규분포로 초기화
+
+    def predict(self, x):
+        return np.do(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+net = simpleNet()
+print(net.W)
---
+++ sig1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+# def step_function(x):
+#     return np.array(x > 0, dtype=np.int)
+#
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = step_function(x)
+#
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+x = np.array([-1.0, 1.0, 2.0])
+sigmoid(x)
+print("Hello World")
